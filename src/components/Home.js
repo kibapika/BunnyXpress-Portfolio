57703d3caa5c8060bae68caa31b9a9e94e9a3edd
Eva Li <evali3101@gmail.com>
2023-11-02 16:55:23 -0400

    added lightbox

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import FsLightbox from "fslightbox-react";
+import mochiBunny from "./img/mochiBunny.png";
 
 const Home = () => {
-    return (
-      <div>
-        <h1>
-          Welcome to BunnyXpress
-        </h1>
-      </div>
-    );
-  };
-  
-  export default Home;
+  const [toggler, setToggler] = useState(false);
+
+  return (
+    <div>
+      <h1>Welcome to BunnyXpress</h1>
+      <button onClick={() => setToggler(!toggler)}>
+        <img
+          className="w-[40%] rounded-xl m-4"
+          alt="mochiBunny"
+          src={mochiBunny}
+        />{" "}
+      </button>
+      <FsLightbox toggler={toggler} sources={[mochiBunny]} />
+    </div>
+  );
+};
+export default Home;
