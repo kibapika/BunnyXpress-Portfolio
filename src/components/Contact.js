280c6647c19fb55ff4694eefab99fb56c3dfdbdc
Eva Li <evali3101@gmail.com>
2023-12-20 21:59:00 -0500

    contact mobile friendly

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-[2.3rem] pb-4">Contact</h1>
-      <p className="text-center text-[1.2rem]">
+    <div className="h-[35vh] sm:h-[100%] w-[100dvw] flex flex-col justify-around items-center">
+      <h1 className="text-[2.3rem] sm:pb-4">Contact</h1>
+      <p className="text-[1.4rem] w-[75%] text-center sm:text-[1.2rem]">
         If you have any question regarding business inquires and any other
         questions, feel free to contact me!
       </p>
-      <address className="pt-4 text-[1.1rem] hover:text-[#eb5e28] transition duration-75">
+      <address className="text-[1.35rem] sm:pt-4 sm:text-[1.1rem] hover:text-[#eb5e28] transition duration-75">
         <a href="mailto:bunnyxpressstickers@gmail.com">
           bunnyxpressstickers@gmail.com
         </a>
