619c15cd3c95ee7f6e8be319feff41a22dc453e8
Eva Li <evali3101@gmail.com>
2023-12-20 22:38:57 -0500

    about mobile friendly

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ const About = () => {
   const [toggler, setToggler] = useState(false);
 
   return (
-    <div className="flex flex-row justify-evenly items-center">
-      <section className="w-[40%]">
-        <h1 className="pb-6 text-center text-[1.8rem] font-extrabold">
+    <div className="flex flex-col sm:flex-row justify-evenly items-center">
+      <section className="w-[80%] sm:w-[40%]">
+        <h1 className="pb-3 sm:pb-6 text-center text-[2rem] sm:text-[1.8rem] font-extrabold">
           Meet The Artist - Eva
         </h1>
-        <p>
+        <p className="text-[1.1rem] sm:text-[1rem]">
           Hello! My name is Eva (BunnyXpress) and I am a new artist based in New
           York! I have always had a passion for drawing, and decided to showcase
           my art with inspiration from my childhood~
@@ -27,7 +27,7 @@ const About = () => {
           very first Etsy shop! I hope the little joys from my life can also
           brighten a little bit of your day~
         </p>
-        <div className="w-[14.5rem] pt-6 flex flex-row justify-between">
+        <div className="sm:w-[14.5rem] py-4 sm:pt-6 flex flex-row justify-evenly sm:justify-between">
           <button to="/contact" className="aboutBtn">
             <NavLink to="/contact">Contact</NavLink>
           </button>
@@ -43,10 +43,10 @@ const About = () => {
         </div>
       </section>
       <button
-        className="w-[30%] flex justify-center items-center"
+        className="w-[60%] sm:w-[30%] flex justify-center items-center"
         onClick={() => setToggler(!toggler)}
       >
-        <img className="max-h-full" alt="lemonTea" src={lemonTea} />{" "}
+        <img alt="lemonTea" src={lemonTea} />{" "}
       </button>
       <FsLightbox toggler={toggler} sources={[lemonTea]} />
     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="h-[35vh] sm:h-[100%] w-[100dvw] flex flex-col justify-around items-center">
-      <h1 className="text-[2.3rem] sm:pb-4">Contact</h1>
+      <h1 className="text-[2.3rem] sm:pb-4 font-extrabold">Contact</h1>
       <p className="text-[1.4rem] w-[75%] text-center sm:text-[1.2rem]">
         If you have any question regarding business inquires and any other
         questions, feel free to contact me!
